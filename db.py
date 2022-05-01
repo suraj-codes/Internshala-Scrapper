73732656550d740296d8be6247b5f9446fecc7d2
Suraj Deshmane <59314239+suraj-codes@users.noreply.github.com>
2022-05-01 12:48:49 +0530

    mongo url hide

--- db.py
+++ db.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_pymongo import pymongo
 from app import app
 
-CONNECTION_STRING = "mongodb+srv://internshala:surajcodes@cluster0.g333f.mongodb.net/internshala?retryWrites=true&w=majority"
+CONNECTION_STRING = process.env.MONGO_URL
 client = pymongo.MongoClient(CONNECTION_STRING)
 db = client.get_database('internshala')
 user_collection = pymongo.collection.Collection(db, 'internships')
